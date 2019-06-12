5b1d89a5d4478511c9bf2478cbbda455e099d9ba
Lorefrance Ubay <FrankenState>
2019-06-12 19:14:12 +0800

    Updates

---
+++ precache-manifest.78524fe0a5089fd89e262941eb889a48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d979b626ca6f300adfaa1835f364a0",
+    "url": "/react-contact-mngr/index.html"
+  },
+  {
+    "revision": "52051b6a1833bb902112",
+    "url": "/react-contact-mngr/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8f7c8bc4729520bd70aa",
+    "url": "/react-contact-mngr/static/js/2.3c72c32f.chunk.js"
+  },
+  {
+    "revision": "52051b6a1833bb902112",
+    "url": "/react-contact-mngr/static/js/main.7cd27fec.chunk.js"
+  },
+  {
+    "revision": "a4da94d9a6f3e1da6ab5",
+    "url": "/react-contact-mngr/static/js/runtime~main.4bfd760f.js"
+  }
+]);
